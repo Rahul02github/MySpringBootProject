6ee2f5d958443b826049770ac84121728d372cb5
Rahul-PC <Rahul-PC@DESKTOP-5LS7R61>
2020-04-11 17:52:45 +0530

    commit

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(101,'Rahul','Java');
+insert into alien values(102,'Rakesh','Python');
+insert into alien values(103,'Mohan','R');
+insert into alien values(104,'Navin','ML');
+insert into alien values(105,'Monu','SQL');
